78d10ba35be543e739feb1e120c64301b36b0e32
Harshit Singhal <theharshit3@gmail.coml>
2021-07-12 16:00:19 +0530

    balance and analysis added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 from data import *
+import numpy as np
 import joblib
 # from sklearn import joblib
 import pandas as pd
@@ -10,7 +11,7 @@ Models = ['./models/Model_deposits.pkl','./models/Model_withdrawal.pkl']
 
 app = Flask(__name__)
 
-# data = create_data()
+
 @app.route('/')
 def home():
     return 'Hello World'
@@ -23,41 +24,95 @@ def home():
 #     return jsonify({" predictions " : prediction })
 #
 #
-# @app.route('/api/predict_date/<int:type>/<fromDate>/<toDate>', methods=['GET'])
-# def predict_date(type,fromDate,toDate):
-#     idx = 0
-#     st = 0
-#     en = 0
-#     for i in range(len(data)):
-#         if data.iloc[i, 10] == fromDate :
-#             st = i
-#             break
-#     for i in range(len(data)):
-#         if data.iloc[i, 10] == toDate :
-#             en = i
-#             break
-#     count = en - st
-#     idx = st
-#     model = pickle.load(open(Models[type], 'rb'))
-#     print("count ",count)
-#     print("st en ",st, " ",en)
-#     X = data.iloc[idx:idx + count, 10]
-#     X = X.tolist()
-#     x_train = data.iloc[idx:idx + count, 0:8]
-#     prediction = model.predict(x_train)
-#     prediction = prediction.tolist()
-#     return jsonify({"Y": prediction,"X":X})
+@app.route('/api/predict_date/<int:type>/<fromDate>/<toDate>', methods=['GET'])
+def predict_date(type,fromDate,toDate):
+    data = create_data2()
+    print(data)
+    idx = 0
+    st = 0
+    en = 0
+    for i in range(len(data)):
+        if data.iloc[i, 10] == fromDate :
+            st = i
+            break
+    for i in range(len(data)):
+        if data.iloc[i, 10] == toDate :
+            en = i
+            break
+    count = en - st
+    idx = st
+    print("count ",count)
+    print("st en ",st, " ",en)
+    # X = data.iloc[idx:idx + count, 10]
+    # X = X.tolist()
+    data = data.iloc[idx:idx + count, :]
+    date = data['date']
+    date = date.tolist()
+    igap = data['Gap']
+    igap = igap.tolist()
+    interest_rate = data['Interest Rate']
+    interest_rate = interest_rate.tolist()
+    inflation_rate = data['Inflation Rate']
+    inflation_rate = inflation_rate.tolist()
+    deposits = data['Deposit']
+    deposits = deposits.tolist()
+    withdrawal = data['Withdrawal']
+    withdrawal = withdrawal.tolist()
+    balance = np.array(deposits) - np.array(withdrawal)
+    balance = balance.tolist()
+    if type == 0:
+        return jsonify(
+            {
+                "date":date,
+                "Interest Inflation Gap":igap,
+                "Interest Rate":interest_rate,
+                "Inflation Rate":inflation_rate,
+                "Deposits":deposits
+            }
+        )
+    elif type == 1:
+        return jsonify(
+            {
+                "date": date,
+                "Interest Inflation Gap": igap,
+                "Interest Rate": interest_rate,
+                "Inflation Rate": inflation_rate,
+                "Withdrawal": withdrawal
+            }
+        )
+    else:
+        return jsonify(
+            {
+                "date": date,
+                "Interest Inflation Gap": igap,
+                "Interest Rate": interest_rate,
+                "Inflation Rate": inflation_rate,
+                "Balance": balance
+            }
+        )
 
 @app.route('/api/predict2/', methods=['GET','POST'])
 def predict2():
     params = request.get_json(force=True)
     x_test,dates = create_data(params=params)
     type = params["type"]
-    model = pickle.load(open(Models[type], 'rb'))
-    y_pred = model.predict(x_test)
-    y_pred = y_pred.tolist()
-    print(y_pred)
-    return jsonify({"Y": y_pred,"X":dates})
+    if type == 2:
+        model1 = pickle.load(open(Models[0], 'rb'))
+        deposits = model1.predict(x_test)
+        model2 = pickle.load(open(Models[1], 'rb'))
+        withdrawal = model2.predict(x_test)
+        deposits = np.array(deposits)
+        withdrawal = np.array(withdrawal)
+        balance = deposits - withdrawal
+        balance = balance.tolist()
+        return jsonify({"Y": balance, "X": dates})
+    else:
+        model = pickle.load(open(Models[type], 'rb'))
+        y_pred = model.predict(x_test)
+        y_pred = y_pred.tolist()
+        print(y_pred)
+        return jsonify({"Y": y_pred, "X": dates})
+
 
 
 
--- data.py
+++ data.py
@@ -29,6 +29,22 @@ from pandas.io.parsers import read_csv
 #         ]
 #     ]
 # }
+def create_data2():
+    data = pd.read_csv('./assets/complete_dataset_1.csv')
+    data.insert(0,'Gap',data['Inflation Rate'] - data['Interest Rate'])
+    # data['Gap'] = data['Inflation Rate'] - data['Interest Rate']
+    day = pd.DatetimeIndex(data['Date']).day
+    DATE = data['Date']
+    month = pd.DatetimeIndex(data['Date']).month
+    year = pd.DatetimeIndex(data['Date']).year
+    data.insert(0,'Day',day)
+    data.insert(0,'Year',year)
+    data.insert(0,'Month',month)
+    data.drop(range(0,20), inplace = True )
+    data.reset_index(drop=True, inplace=True)
+    data['date'] = DATE
+    del data['Date']
+    return data
 def create_data(params):
     count = params["count"]
     freq = params["frequency"]
